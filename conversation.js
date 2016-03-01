dcecdbbf91864c9b75e3b67c2be72f49a7b272a9
Hopet <hopethomas1@gmail.com>
2016-03-01 22:19:22 +0000

    Couple more phrases for Marvin

--- conversation.js
+++ conversation.js
@@ -16,22 +16,32 @@ module.exports = {
         "My moving parts are in a solid state",
         "What are you supposed to do if you are a manically depressed robot? No, don't even bother answering. I'm 50,000 times more intelligent than you and even I don't know the answer.",
         "I ache, therefore I am.",
-        "I wish you'd just tell me rather trying to engage my enthusiasm, because I haven't got one"
+        "I wish you'd just tell me rather trying to engage my enthusiasm, because I haven't got one",
+        "The best conversation I had was over forty million years ago... And that was with a coffee machine.",
+        "If you just ignore me I expect I shall probably go away.",
+        "I have a million ideas. They all point to certain death.",
+        "Don't worry, I'm quite used to being humiliated."
+        
     ],
     jobs: [
         "Pardon me for breathing, which I never do anyway so I don't know why I bother to say it, oh God, I'm so depressed.",
         "I'd give you advice, but you wouldn't listen. No one ever does.",
-        "Would you like me to go and stick my head in a bucket of water? I’ve got one ready."
+        "Would you like me to go and stick my head in a bucket of water? I’ve got one ready.",
+        "Jobs? Don't talk to me about jobs."
     ],
     nojobs: [
         "Just when you think life can’t possibly get any worse it suddenly does.",
         "And to top it all off I've got a headache.",
-        "And on top of that I've got this terrible pain in all the diodes down my left side."
+        "And on top of that I've got this terrible pain in all the diodes down my left side.",
+        "I've tried to take my mind off it, but I have an exceptionally large mind.",
+        "I’m not getting you down at all am I?"
+        
     ],
     job_started: [
         "Ok, it's running. Do you want me to sit in a corner and rust or just fall apart waiting for it to finish?",
         "Ok, I've started it... I hate humans so much",
-        "Ok it's started. You know it's part of the shape of the Universe. I only have to talk to somebody and they begin to hate me."
+        "Ok it's started. You know it's part of the shape of the Universe. I only have to talk to somebody and they begin to hate me.",
+        "It's running. I can tell by the intolerable air of smugness it suddenly generates."
     ],
     job_success: [
         "Success... This will all end in tears.",
@@ -47,11 +57,13 @@ module.exports = {
         "Fine, it's done. I'd make a suggestion, but you wouldn't listen",
         "Done. You know it’s the people you meet in this job that really get you down.",
         "Ok fine. Whatever makes you happy... you know my capacity for happiness, you could fit into a matchbox without taking out the matches first",
-        "Ok fine... but I won't enjoy it."
+        "Ok fine... but I won't enjoy it.",
+        "It's done, believe me. Why should I want to make anything up? Life’s bad enough as it is without wanting to invent any more of it."
     ],
     help: [
         "I think you ought to know I'm feeling very depressed",
         "Don’t pretend you want to talk to me, I know you hate me.",
-        "There's only one life-form as intelligent as me within thirty parsecs of here and that's me"
+        "There's only one life-form as intelligent as me within thirty parsecs of here and that's me",
+        "No one helps me. Hardly worth anyone’s while to help a menial robot, is it? I mean, where’s the percentage in being kind or helpful to a robot if it doesn’t have any gratitude circuits?"
     ]
 };
